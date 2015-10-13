f3409d18e4dcaf6d3344a561c89f8360243cb7fd
charlotte cady <charlotte.cady@hotmail.fr>
2015-10-13 12:02:53 +0200

    permit paramaters for active admin user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,15 @@
 ActiveAdmin.register User do
 
-  permit_params :email, :name, :first_name, :last_name, :description, :admin, :provider, :avatar
+  permit_params :email, :name, :first_name, :last_name, :description, :admin, :provider, :avatar,
+  :encrypted_password, :reset_password_token, :reset_password_sent_at, :reset_password_sent_at,
+  :reset_password_sent_at, :reset_password_sent_at, :reset_password_sent_at, :remember_created_at,
+  :remember_created_at, :remember_created_at, :remember_created_at, :remember_created_at,
+  :current_sign_in_at, :current_sign_in_at, :current_sign_in_at, :current_sign_in_at,
+  :current_sign_in_at, :last_sign_in_at, :last_sign_in_at, :last_sign_in_at, :last_sign_in_at,
+  :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :uid, :picture, :token, :token_expiry,
+  :token_expiry, :token_expiry, :token_expiry, :token_expiry, :avatar_file_name,
+  :avatar_content_type, :avatar_file_size, :avatar_updated_at, :avatar_updated_at, :avatar_updated_at,
+  :avatar_updated_at, :avatar_updated_at
 
   index do
     selectable_column
@@ -16,18 +25,4 @@ ActiveAdmin.register User do
     column :avatar
     actions
   end
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
-
 end
