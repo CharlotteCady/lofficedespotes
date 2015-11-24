07a14dd6251f55ea4c4b86e931c3cc4a8a34dbed
charlotte cady <charlotte.cady@hotmail.fr>
2015-11-24 18:31:34 +0100

    delete image to chrome home page

--- app/assets/javascripts/app/home_video.js
+++ app/assets/javascripts/app/home_video.js
@@ -1,10 +1,10 @@
-var isChrome = !!window.chrome;
-var isIE = /*@cc_on!@*/false;
+// var isChrome = !!window.chrome;
+// var isIE = /*@cc_on!@*/false;
 
-if( isChrome ) {
-$("#dream-job-video").replaceWith($('<img src="http://lofficedespotes.herokuapp.com/app/assets/images/dream.jpg" class="bg" />'));
-}
+// if( isChrome ) {
+// $("#dream-job-video").replaceWith($('<img src="assets/dream.jpg" class="bg" />'));
+// }
 
-if( isIE ) {
-$("#dream-job-video").replaceWith($('<img id="dream-job-video" src="images/dream.jpg" />'));
-}
+// if( isIE ) {
+// $("#dream-job-video").replaceWith($('<img src="images/dream.jpg" />'));
+// }
