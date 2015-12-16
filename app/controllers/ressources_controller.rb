842142412fe93836f93720ace5b2616738a3d5c9
charlotte cady <charlotte.cady@hotmail.fr>
2015-12-16 13:41:47 +0100

    disqus for staging

--- app/controllers/ressources_controller.rb
+++ app/controllers/ressources_controller.rb
@@ -118,9 +118,9 @@ class RessourcesController < ApplicationController
     if request.host == "localhost"
       @url_host = "http://localhost:3000"
     elsif request.host == "lofficedespotes-staging.herokuapp.com"
-      @url_host = "https://lofficedespotes-staging.herokuapp.com/"
+      @url_host = "https://lofficedespotes-staging.herokuapp.com"
     else
-      @url_host = "http://lofficedespotes.herokuapp.com/"
+      @url_host = "http://lofficedespotes.herokuapp.com"
     end
   end
 
