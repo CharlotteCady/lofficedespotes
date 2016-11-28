dcd98b5945e171287b734518f31a9c8cea05ffac
charlotte cady <charlotte.cady@hotmail.fr>
2016-11-28 17:33:07 +0100

    modification des ID des liens des pages show

--- app/assets/javascripts/app/track_link_content_show.js
+++ app/assets/javascripts/app/track_link_content_show.js
@@ -1,19 +1,32 @@
+// RAJOUTER UN ID AVEC INDICE POUR CHAQUE LIEN CRÉÉ
+// $(document).ready(function() {
+//   if ($('body').is('#ressources_show')) {
+//     var linkNumber = $('div.content-text a').length
+//     for (var i = 1; i < linkNumber + 1; i++) {
+//       var compteur = i;
+//       $('div.content-text a:eq(' + (i - 1) + ')').attr('id', 'ressource-content-website' + [i]);
+//     }
+//   }
+// })
+
+// $(document).ready(function() {
+//   if ($('body').is('#articles_show')) {
+//     var linkNumber = $('div.content-text a').length
+//     for (var i = 1; i < linkNumber + 1; i++) {
+//       var compteur = i;
+//       $('div.content-text a:eq(' + (i - 1) + ')').attr('id', 'article-content-website' + [i]);
+//     }
+//   }
+// })
+
 $(document).ready(function() {
   if ($('body').is('#ressources_show')) {
-    var linkNumber = $('div.content-text a').length
-    for (var i = 1; i < linkNumber + 1; i++) {
-      var compteur = i;
-      $('div.content-text a:eq(' + (i - 1) + ')').attr('id', 'ressource-content-website' + [i]);
-    }
+    $('div.content-text a').attr('id', 'ressource-content-website');
   }
 })
 
 $(document).ready(function() {
   if ($('body').is('#articles_show')) {
-    var linkNumber = $('div.content-text a').length
-    for (var i = 1; i < linkNumber + 1; i++) {
-      var compteur = i;
-      $('div.content-text a:eq(' + (i - 1) + ')').attr('id', 'article-content-website' + [i]);
-    }
+    $('div.content-text a').attr('id', 'article-content-website');
   }
 })
