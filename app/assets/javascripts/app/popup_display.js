cd742dcb5ca8bc7500cf12f9d07d04bc94fea7a1
charlotte cady <charlotte.cady@hotmail.fr>
2016-12-06 12:14:57 +0100

    popup NL par session

--- app/assets/javascripts/app/popup_display.js
+++ app/assets/javascripts/app/popup_display.js
@@ -1,12 +1,12 @@
 // AFFICHER LA NL POUR LA 1ERE VISITE
-$(document).ready(function() {
-  if(localStorage.getItem('popState') != 'shown'){
-    setTimeout(function() {
-      $('#myModal').modal({show: true, keyboard: true});
-      }, 6000); // milliseconds
-    localStorage.setItem('popState','shown')
-  }
-});
+//$(document).ready(function() {
+//  if(localStorage.getItem('popState') != 'shown'){
+//    setTimeout(function() {
+//      $('#myModal').modal({show: true, keyboard: true});
+//      }, 6000); // milliseconds
+//    localStorage.setItem('popState','shown')
+//  }
+//});
 
 
 // AFFICHER LA NL QUAND LA SOURIS SORT DE LA PAGE
@@ -14,9 +14,12 @@ $(document).ready(function() {
 //   $('#myModal').modal({show: true, keyboard: true});
 // });
 
-// AFFICHER LA NL AU BOUT DE 1 MINUTE 30
+// AFFICHER LA NL AU BOUT DE 1 MINUTE 30 à chaque session
 $(document).ready(function() {
-  setTimeout(function() {
-    $('#myModal').modal({show: true, keyboard: true});
-    }, 90000); // milliseconds
+  if(sessionStorage.getItem('popState') != 'shown'){
+    setTimeout(function() {
+      $('#myModal').modal({show: true, keyboard: true});
+      }, 6000); // milliseconds
+    sessionStorage.setItem('popState','shown')
+  }
 });
