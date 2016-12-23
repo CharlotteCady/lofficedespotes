265ca8ff249da35d2fd50b93a8660e02596aa6f4
charlotte cady <charlotte.cady@hotmail.fr>
2016-12-23 16:49:09 +0100

    reco des jobbeurs

--- app/assets/javascripts/app/home-title-animation.js
+++ app/assets/javascripts/app/home-title-animation.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $(function(){
     $(".animate-element").typed({
-      strings: ["Décrocher un job", "Trouver votre voie", "Changer de métier", "Changer de boite", "Créer votre activité"],
+      strings: ["décrocher un job", "trouver votre voie", "changer de métier", "changer de boite", "créer votre activité"],
       typeSpeed: 50,
       backSpeed: 50,
       backDelay: 4000,
