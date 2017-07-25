0afee546d2c17dd520dbd15ce724a9821bd99b9b
Charlotte Cady <charlotte.cady@hotmail.fr>
2017-07-25 21:23:10 +0200

    postmark fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,16 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  
+  
+  # ---- Initial config before Postmark addon remove from heroku
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
+
+  config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
+  # config.action_mailer.default_url_options = { host: "yourapp.herokuapp.com" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
